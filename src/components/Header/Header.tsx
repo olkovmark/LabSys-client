3c3c51d7aded7905450bb67fe952197f7caf2ea3
olkovmark <olkovmark@gmail.com>
2023-07-22 18:46:43 +0200

    update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,11 +6,15 @@ import Image from "next/image";
 import logo from "@/assets/images/logo.svg";
 import logoutImg from "@/assets/images/icons/logout.svg";
 import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Header() {
   const router = useRouter();
   const navLinks = navigationsLinks;
   const currentRoute = usePathname();
+  const currentRouteIndex = navigationsLinks.findIndex(
+    (v) => v.url === currentRoute
+  );
 
   return (
     <div className={styles.header}>
@@ -28,12 +32,16 @@ export default function Header() {
             key={i}
             href={v.url}
             className={`${styles.nav_item} ${
-              currentRoute === v.url ? styles.nav_active : ""
+              i === currentRouteIndex ? styles.nav_active : ""
             }`}
           >
             {v.name}
           </Link>
         ))}
+        <div
+          className={styles.line}
+          style={{ left: `${110 * currentRouteIndex + 10}px` }}
+        ></div>
       </nav>
       <button
         className={styles.logout}
@@ -49,6 +57,19 @@ export default function Header() {
 }
 
 export const navigationsLinks = [
+  {
+    name: "Analyses",
+    url: "/analyses",
+  },
+  {
+    name: "Patients",
+    url: "/patients",
+  },
+
+  {
+    name: "Settings",
+    url: "/settings",
+  },
   {
     name: "Analyses",
     url: "/analyses",
